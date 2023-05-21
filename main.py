0a864f729e971107944aecd223c8a56166b2b74a
PavloKryshchyshyn <inshe.imia@gmail.com>
2023-05-21 22:21:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    with open('films.json') as file:
+        data = json.load(file)
+
+    return render_template('index.html', films=data)
+
+if __name__ == '__main__':
+    app.run()
