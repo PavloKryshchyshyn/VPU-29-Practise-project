9348596c61ccaeddefb34ce87798bbb636de1bfa
unknown <inshe.imia@gmail.com>
2023-05-29 10:20:54 +0300

    Додано стилі

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
